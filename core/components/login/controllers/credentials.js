c6939b0c659d818b1d7f83b6233745529d9d4c0e
Raleduc <32550785+Raleduc@users.noreply.github.com>
2017-10-09 13:56:29 -0300

    Update credentials.js

--- core/components/login/controllers/credentials.js
+++ core/components/login/controllers/credentials.js
@@ -168,12 +168,12 @@ angular.module('mm.core.login')
                             // Action should only have 1 site because we're filtering by username.
                             action.action(action.sites[0]);
                         } else {
-                            //return $state.go('site.mm_courses');
+                            
                             return $mmLoginHelper.goToSiteInitialPage();
                         }
                     });
                 } else {
-                    //return $state.go('site.mm_courses');
+                    
                   return $mmLoginHelper.goToSiteInitialPage();
                 }
             });
